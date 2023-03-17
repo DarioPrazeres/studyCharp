f478c350872938d36f3d7b63921507889a4889df
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-03-17 22:54:37 +0100

    update exercice

--- Exercice/exercice11/Program.cs
+++ Exercice/exercice11/Program.cs
@@ -19,7 +19,15 @@ namespace Exercice
 {
     class Exercice11{
         static void Main(string[] agrs){
-            Console.WriteLine("Hello");
+            int  timeSecond,hour, minute, second;
+            Console.Write("Insert Time in second: ");
+            timeSecond = Convert.ToInt16(Console.ReadLine());
+            hour = timeSecond/3600;
+            minute = (timeSecond/60)%10;
+            second = timeSecond/60;
+            Console.WriteLine("Time In Hour is: {0}", hour);
+            Console.WriteLine("Time In Minutes is: {0}", minute);
+            Console.WriteLine("Time In Seconds is: {0}", second);
         }
     }
 }
