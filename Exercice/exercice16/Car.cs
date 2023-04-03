39f625d2f0fbcccc124778632ec7f01bb0717801
Dário Prazeres <87315735+DarioPrazeres@users.noreply.github.com>
2023-04-03 20:05:40 +0100

    update c sharp exercice

---
+++ Exercice/exercice16/Car.cs
@@ -0,0 +1,9 @@
+class Car{
+    private string name;
+    public Car(string Name){
+        name = Name;
+    }
+    public void nameCar(){
+        Console.WriteLine("Eu sou o car {0}", name);
+    }
+}
---
+++ Exercice/exercice16/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Exercice
+{
+    class Exercice14{
+        static void Main(string[] agrs){
+            Car Ford = new Car("Ford");
+            Ford.nameCar();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
